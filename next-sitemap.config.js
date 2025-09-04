6dc83a8faef4c73cb93039f0f956664e6cf96195
oleksijermantraut <mawabek43@gmail.com>
2025-09-04 22:15:10 +0300

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://avanfish.com',
-    generateRobotsTxt: true, 
-    exclude: ['/server-sitemap.xml'], 
-    sitemapSize: 7000,
-  };
+  siteUrl: 'https://avanfish.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
